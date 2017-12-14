c257fd6d85973f68fab387723d4aa5b439fab5fb
Petr Faltýnek <pfaltynek@gmail.com>
2017-12-14 13:59:00 +0100

    finished day 11 solution

--- day11/main.cpp
+++ day11/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -5,7 +6,24 @@
 #include <string>
 #include <vector>
 
+// hexagon grids implemntation theory ...
+// https://www.redblobgames.com/grids/hexagons/
+
 #define TEST 0
+
+/*
+class Hex{
+	public:
+	  int GetX(){
+		  return _x;
+	  };
+	  Hex(int x, int y, int z){
+		assert (x + y + z == 0);
+	  };
+
+	private:
+	  int _x, _y_, _z;
+};*/
 
 typedef enum { N, S, NE, NW, SE, SW } HexWay;
 
@@ -38,6 +56,53 @@ bool IsHexWay(std::string input, HexWay &hex_way) {
 	return false;
 }
 
+HexWay GetOpositeWay(HexWay way) {
+	switch (way) {
+		case HexWay::N:
+			return HexWay::S;
+			break;
+		case HexWay::S:
+			return HexWay::N;
+			break;
+		case HexWay::NE:
+			return HexWay::SW;
+			break;
+		case HexWay::NW:
+			return HexWay::SE;
+			break;
+		case HexWay::SW:
+			return HexWay::NE;
+			break;
+		case HexWay::SE:
+			return HexWay::NW;
+			break;
+		default:
+			return HexWay::N;
+			break;
+	}
+}
+
+void OptimizeInput(std::vector<HexWay> &input) {
+	unsigned int idx = 0;
+	HexWay oposite;
+	bool removed = false;
+
+	while (idx < (input.size() - 1)) {
+		oposite = GetOpositeWay(input[idx]);
+		removed = false;
+		for (unsigned int i = idx + 1; i < input.size(); i++) {
+			if (input[i] == oposite) {
+				input.erase(input.begin() + i);
+				input.erase(input.begin() + idx);
+				break;
+			}
+		}
+		if (!removed) {
+			idx++;
+		}
+	}
+}
+
 std::vector<HexWay> ParseInput(std::string input) {
 	std::vector<HexWay> result;
 	size_t pos;
@@ -66,6 +131,54 @@ std::vector<HexWay> ParseInput(std::string input) {
 
 	return result;
 }
+
+int GetHexagonsDistance(int x1, int y1, int z1, int x2 = 0, int y2 = 0, int z2 = 0) {
+	return (abs(x2 - x1) + abs(y2 - y1) + abs(z2 - z1)) / 2;
+}
+
+void TraceHexWays(std::vector<HexWay> input, int &result1, int &result2) {
+	int x1, x2, y1, y2, z1, z2;
+
+	x1 = y1 = z1 = x2 = y2 = z2 = result2 = 0;
+
+	for (unsigned int i = 0; i < input.size(); i++) {
+		switch (input[i]) {
+			case HexWay::N:
+				y1++;
+				z1--;
+				break;
+			case HexWay::S:
+				y1--;
+				z1++;
+				break;
+			case HexWay::NE:
+				x1++;
+				z1--;
+				break;
+			case HexWay::NW:
+				x1--;
+				y1++;
+				break;
+			case HexWay::SW:
+				x1--;
+				z1++;
+				break;
+			case HexWay::SE:
+				x1++;
+				y1--;
+				break;
+		}
+
+		result1 = GetHexagonsDistance(x1, y1, z1);
+		if (result1 > result2) {
+			x2 = x1;
+			y2 = y1;
+			z2 = z1;
+			result2 = result1;
+		}
+	}
+}
+
 
 int main(void) {
 	int result1 = 0, result2 = 0;
@@ -98,6 +211,11 @@ int main(void) {
 	if (input.is_open()) {
 		input.close();
 	}
+
+	std::vector<HexWay> hex_ways = ParseInput(stream);
+	// do not optimize due part 2 task
+	// OptimizeInput(hex_ways);
+	TraceHexWays(hex_ways, result1, result2);
 
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
