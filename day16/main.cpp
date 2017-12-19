1c0fc5145c7ecf828e853b593d3fcff531aa6c0b
Petr Faltýnek <pfaltynek@gmail.com>
2017-12-19 11:18:35 +0100

    finished day 16 solution

--- day16/main.cpp
+++ day16/main.cpp
@@ -8,74 +8,111 @@
 
 #define TEST 0
 
-const long long factorA = 16807ll;
-const long long factorB = 48271ll;
-const long long divider = 2147483647ll;
+#if TEST
+#define PROGRAMS_MAX 5
+#else
+#define PROGRAMS_MAX 16
+#endif
 
-std::regex line_template("^Generator (A|B) starts with (\\d+)$");
+std::regex spin_template("^s(\\d+)$");
+std::regex swap_by_idx_template("^x(\\d+)\/(\\d+)$");
+std::regex swap_by_name_template("^p([a-p])\/([a-p])$");
 
-bool ParseLine(std::string line, bool &genA, int &start_val) {
-	std::smatch sm;
+std::vector<std::string> Split(std::string to_split, const std::string &delimiter) {
+	std::vector<std::string> result;
+	size_t pos;
 
-	if (regex_match(line, sm, line_template)) {
-		genA = (sm.str(1).compare("A") == 0);
-		start_val = stoi(sm.str(2));
-		return true;
-	}
-	return false;
-}
+	result.clear();
 
-long long GetPairsCount(long long rounds, int startA, int startB) {
-	int a, b;
-	long long result = 0;
-
-	a = startA;
-	b = startB;
-
-	for (long long i = 0; i < rounds; i++) {
-		a = (a * factorA) % divider;
-		b = (b * factorB) % divider;
-
-		if ((a & 0xFFFF) == (b & 0xFFFF)) {
-			result++;
+	pos = to_split.find(delimiter, 0);
+	while (pos != std::string::npos) {
+		if (pos) {
+			result.push_back(to_split.substr(0, pos));
 		}
+		to_split = to_split.substr(pos + delimiter.size(), std::string::npos);
+
+		pos = to_split.find(delimiter, 0);
+	}
+
+	if (to_split.size()) {
+		result.push_back(to_split);
 	}
 
 	return result;
 }
 
-long long GetPairs2Count(long long rounds, int startA, int startB) {
-	int a, b;
-	long long result = 0, cnt = 0;
+bool IsProgramPositionValid(size_t pos) {
+	return (pos >= 0) && (pos < PROGRAMS_MAX);
+}
 
-	a = startA;
-	b = startB;
+void SwapPrograms(std::string &programs, size_t pos1, size_t pos2) {
+	char temp;
 
-	for (long long i = 0; i < rounds; i++) {
-		do {
-			a = (a * factorA) % divider;
-		} while (a % 4);
-		do {
-			b = (b * factorB) % divider;
-		} while (b % 8);
+	temp = programs[pos1];
+	programs[pos1] = programs[pos2];
+	programs[pos2] = temp;
+}
 
-		if (((a % 4) == 0) && ((b % 8) == 0)) {
-			if ((a & 0xFFFF) == (b & 0xFFFF)) {
-				result++;
+std::string LetProgramsDance(std::vector<std::string> dance_moves, long long count) {
+	std::string programs;
+	std::smatch sm;
+	size_t pos1, pos2;
+	std::string first;
+
+	programs.clear();
+
+	for (int i = 0; i < PROGRAMS_MAX; i++) {
+		programs += (char)('a' + i);
+	}
+
+	first = programs;
+
+	for (long long j = 1; j <= count; j++) {
+		for (unsigned int i = 0; i < dance_moves.size(); i++) {
+			if (std::regex_match(dance_moves[i], sm, spin_template)) {
+				pos1 = stoi(sm.str(1));
+				if (IsProgramPositionValid(pos1)) {
+					programs = programs.substr(programs.size() - pos1) + programs.substr(0, programs.size() - pos1);
+				} else {
+					std::cout << "Unable to dance move '" << dance_moves[i] << "'" << std::endl;
+				}
+			} else if (std::regex_match(dance_moves[i], sm, swap_by_idx_template)) {
+				pos1 = stoi(sm.str(1));
+				pos2 = stoi(sm.str(2));
+				if (IsProgramPositionValid(pos1) && IsProgramPositionValid(pos2)) {
+					SwapPrograms(programs, pos1, pos2);
+				} else {
+					std::cout << "Unable to dance move '" << dance_moves[i] << "'" << std::endl;
+				}
+			} else if (std::regex_match(dance_moves[i], sm, swap_by_name_template)) {
+				pos1 = programs.find(sm.str(1), 0);
+				pos2 = programs.find(sm.str(2), 0);
+				if (IsProgramPositionValid(pos1) && IsProgramPositionValid(pos2)) {
+					SwapPrograms(programs, pos1, pos2);
+				} else {
+					std::cout << "Unable to dance move '" << dance_moves[i] << "'" << std::endl;
+				}
+			} else {
+				std::cout << "Unable to dance move '" << dance_moves[i] << "'" << std::endl;
 			}
 		}
-	}
+		if (first.compare(programs) == 0) {
+			long long rest;
 
-	return result;
+			rest = count % j;
+			j = count - rest;
+		}
+	}
+	return programs;
 }
 
 int main(void) {
-	int genA_start, genB_start, cnt = 0;
-	long long result1 = 0, result2 = 0;
+	std::string result1, result2;
 	std::ifstream input;
-	std::string line;
-	bool genA = false, genB = false;
-	std::cout << "=== Advent of Code 2017 - day 15 ====" << std::endl;
+	std::string line, stream;
+	std::vector<std::string> list;
+
+	std::cout << "=== Advent of Code 2017 - day 16 ====" << std::endl;
 	std::cout << "--- part 1 ---" << std::endl;
 
 #if TEST
@@ -89,34 +126,22 @@ int main(void) {
 		return -1;
 	}
 
-	while (std::getline(input, line)) {
-		int x;
-		bool y;
-		cnt++;
-		if (!ParseLine(line, y, x)) {
-			std::cout << "Invalid program info on line " << cnt << std::endl;
-		} else {
-			if (y) {
-				genA = true;
-				genA_start = x;
-			} else {
-				genB = true;
-				genB_start = x;
-			}
-		}
-	}
+	stream.clear();
+	list.clear();
 
-	if (!genA || !genB) {
-		std::cout << "Incomplette input data" << cnt << std::endl;
-		return -1;
+	while (std::getline(input, line)) {
+		stream.append(line);
+		stream.append(",");
 	}
 
 	if (input.is_open()) {
 		input.close();
 	}
 
-	result1 = GetPairsCount(40000000ll, genA_start, genB_start);
-	result2 = GetPairs2Count(5000000ll, genA_start, genB_start);
+	list = Split(stream, ",");
+
+	result1 = LetProgramsDance(list, 1);
+	result2 = LetProgramsDance(list, 1000000000);
 
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
