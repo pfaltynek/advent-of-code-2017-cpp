df7bb12c62760f88a4c29e0740c600c35937fa93
Petr Faltýnek <pfaltynek@gmail.com>
2018-01-04 13:46:21 +0100

    finished day 21 solution

--- day21/main.cpp
+++ day21/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -6,7 +7,7 @@
 #include <string>
 #include <vector>
 
-#define TEST 1
+#define TEST 0
 
 std::regex size2rule_template("^([.#]{2}\\/[.#]{2}) => ([.#]{3})\\/([.#]{3})\\/([.#]{3})$");
 std::regex size3rule_template("^([.#]{3}\\/[.#]{3}\\/[.#]{3}) => ([.#]{4})\\/([.#]{4})\\/([.#]{4})\\/([.#]{4})$");
@@ -99,7 +100,7 @@ std::vector<std::string> SplitVertically(std::vector<std::string> input) {
 
 bool ParseRule(std::string line, std::map<std::string, std::vector<std::string>> &rules, std::map<std::string, std::string> &variants) {
 	std::smatch sm;
-	std::vector<std::string> output, lines, lines2;
+	std::vector<std::string> output, lines, lines2, lines3;
 	std::string input;
 
 	output.clear();
@@ -121,30 +122,122 @@ bool ParseRule(std::string line, std::map<std::string, std::vector<std::string>>
 
 	rules[input] = output;
 	lines = Split(input, "/");
-
+	variants[input] = input;
+	// h-split
+	lines2 = SplitHorizontally(lines);
+	variants[Join(lines2, "/")] = input;
+	//v-split
+	lines2 = SplitVertically(lines);
+	variants[Join(lines2, "/")] = input;
+	// v&h-split
+	lines2 = SplitHorizontally(lines2);
+	variants[Join(lines2, "/")] = input;
+	// rotate right
+	lines2 = RotateClockwise(lines);
+	variants[Join(lines2, "/")] = input;
+	lines3 = SplitHorizontally(lines2);
+	variants[Join(lines3, "/")] = input;
+	lines3 = SplitVertically(lines2);
+	variants[Join(lines3, "/")] = input;
+	// rotate left
+	lines2 = RotateCounterClockwise(lines);
+	variants[Join(lines2, "/")] = input;
+	lines3 = SplitHorizontally(lines2);
+	variants[Join(lines3, "/")] = input;
+	lines3 = SplitVertically(lines2);
+	variants[Join(lines3, "/")] = input;
 	return true;
 }
 
-std::vector<std::string> IteratePattern(std::vector<std::string> pattern, std::map<std::string, std::vector<std::string>> rules, unsigned int iterations) {
-	std::vector<std::string> new_pattern;
+std::vector<std::string> IteratePattern(std::vector<std::string> pattern, std::map<std::string, std::vector<std::string>> rules,
+										std::map<std::string, std::string> variants, unsigned int iterations) {
+	std::vector<std::string> new_pattern, part;
+	std::string input;
+	unsigned int blocks;
+
+	for (unsigned int i = 0; i < iterations; ++i) {
+		new_pattern.clear();
+
+		if ((pattern.size() % 2) == 0) {
+
+			blocks = pattern.size() / 2;
+			new_pattern.resize(blocks * 3);
+
+			for (unsigned int j = 0; j < blocks; ++j) {
+				for (unsigned int k = 0; k < blocks; ++k) {
+					part.clear();
+					part.push_back(pattern[j * 2].substr(k * 2, 2));
+					part.push_back(pattern[(j * 2) + 1].substr(k * 2, 2));
+					input = Join(part, "/");
+					if (variants.find(input) == variants.end()) {
+						// problem ???
+						int z = 78;
+					} else {
+						input = variants[input];
+						new_pattern[3 * j].append(rules[input][0]);
+						new_pattern[(3 * j) + 1].append(rules[input][1]);
+						new_pattern[(3 * j) + 2].append(rules[input][2]);
+					}
+				}
+			}
+		} else if ((pattern.size() % 3) == 0) {
+
+			blocks = pattern.size() / 3;
+			new_pattern.resize(blocks * 4);
+
+			for (unsigned int j = 0; j < blocks; ++j) {
+				for (unsigned int k = 0; k < blocks; ++k) {
+					part.clear();
+					part.push_back(pattern[j * 3].substr(k * 3, 3));
+					part.push_back(pattern[(j * 3) + 1].substr(k * 3, 3));
+					part.push_back(pattern[(j * 3) + 2].substr(k * 3, 3));
+					input = Join(part, "/");
+					if (variants.find(input) == variants.end()) {
+						// problem ???
+						int z = 78;
+					} else {
+						input = variants[input];
+						new_pattern[4 * j].append(rules[input][0]);
+						new_pattern[(4 * j) + 1].append(rules[input][1]);
+						new_pattern[(4 * j) + 2].append(rules[input][2]);
+						new_pattern[(4 * j) + 3].append(rules[input][3]);
+					}
+				}
+			}
+		} else {
+			// problem???
+			int z = 13;
+		}
+
+		pattern = new_pattern;
+	}
 
 	return new_pattern;
 }
 
+int CalculateOnPixels(std::vector<std::string> pattern) {
+	std::string line;
+
+	line = Join(pattern, "/");
+	return std::count(line.begin(), line.end(), '#');
+}
+
 int main(void) {
-	int cnt = 0, result1 = 0, result2 = 0;
+	int cnt = 0, result1 = 0, result2 = 0, iterations = 0;
 	std::ifstream input;
 	std::string line;
 	std::map<std::string, std::vector<std::string>> rules;
 	std::map<std::string, std::string> variants;
 	std::cout << "=== Advent of Code 2017 - day 21 ====" << std::endl;
 	std::cout << "--- part 1 ---" << std::endl;
-	std::vector<std::string> pattern;
+	std::vector<std::string> pattern, tt;
 
 #if TEST
 	input.open("input-test.txt", std::ifstream::in);
+	iterations = 2;
 #else
 	input.open("input.txt", std::ifstream::in);
+	iterations = 5;
 #endif
 
 	if (input.fail()) {
@@ -171,7 +264,14 @@ int main(void) {
 		input.close();
 	}
 
+	result1 = CalculateOnPixels(IteratePattern(pattern, rules, variants, iterations));
+
+
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
+
+	iterations = 18;
+	result2 = CalculateOnPixels(IteratePattern(pattern, rules, variants, iterations));
+
 	std::cout << "Result is " << result2 << std::endl;
 }
