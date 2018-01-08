f887e485a28882d5db256995ca1c6a81a5400a28
Petr Faltýnek <pfaltynek@gmail.com>
2018-01-08 15:20:15 +0100

    finished day 24 part 1 solution

--- day24/main.cpp
+++ day24/main.cpp
@@ -11,28 +11,76 @@
 
 std::regex component_template("^(\\d+)\\/(\\d+)$");
 
-void AddEmptyListIfNeeded(std::map<int, std::map<int, std::string>> &components, int val) {
+class component {
+  public:
+	std::string desc;
+	int first, second;
+};
+
+void AddEmptyListIfNeeded(std::map<int, std::map<int, component>> &components, int val) {
 	if (components.find(val) == components.end()) {
-		std::map<int, std::string> list;
+		std::map<int, component> list;
 		list.clear();
 		components[val] = list;
 	}
 }
 
-bool ParseComponent(std::string line, std::map<int, std::map<int, std::string>> &components) {
+bool ParseComponent(std::string line, std::map<int, std::map<int, component>> &components) {
 	std::smatch sm;
 
 	if (std::regex_match(line, sm, component_template)) {
+		component comp;
 		int l, r;
+
 		l = stoi(sm.str(1));
 		r = stoi(sm.str(2));
 		AddEmptyListIfNeeded(components, l);
 		AddEmptyListIfNeeded(components, r);
-		components[l][r] = line;
-		components[r][l] = line;
+		comp.desc = line;
+		comp.first = l;
+		comp.second = r;
+		components[l][r] = comp;
+		comp.first = r;
+		comp.second = l;
+		components[r][l] = comp;
 		return true;
 	} else {
 		return false;
+	}
+}
+
+void FindStrongestBridge(std::vector<component> &bridge, std::map<int, std::map<int, component>> &components, int sum, unsigned int &longest, int &result1,
+						 int &result2) {
+	int start;
+
+	if (bridge.size()) {
+		start = bridge.back().second;
+	} else {
+		start = 0;
+	}
+
+	if (components.find(start) != components.end()) {
+		if (components[start].size()) {
+			for (auto it = components[start].begin(); it != components[start].end(); it++) {
+				std::map<int, std::map<int, component>> cn(components);
+				std::vector<component> bn(bridge);
+				int end = it->second.second;
+				int sn;
+
+				sn = sum + start + end;
+				bn.push_back(it->second);
+				cn[start].erase(end);
+				cn[end].erase(start);
+				if (sn > result1) {
+					result1 = sn;
+				}
+				if (longest <= bn.size()) {
+					longest = bn.size();
+					result2 = sn;
+				}
+				FindStrongestBridge(bn, cn, sn, longest, result1, result2);
+			}
+		}
 	}
 }
 
@@ -41,7 +89,9 @@ int main(void) {
 	int result1 = 0, result2 = 0;
 	std::ifstream input;
 	std::string line;
-	std::map<int, std::map<int, std::string>> components;
+	std::map<int, std::map<int, component>> components;
+	std::vector<component> bridge;
+	unsigned int longest = 0;
 
 	std::cout << "=== Advent of Code 2017 - day 24 ====" << std::endl;
 	std::cout << "--- part 1 ---" << std::endl;
@@ -58,6 +108,7 @@ int main(void) {
 	}
 
 	components.clear();
+	bridge.clear();
 
 	while (std::getline(input, line)) {
 		cnt++;
@@ -69,6 +120,8 @@ int main(void) {
 	if (input.is_open()) {
 		input.close();
 	}
+
+	FindStrongestBridge(bridge, components, 0, longest, result1, result2);
 
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
